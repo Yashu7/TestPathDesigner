2b478afb7365f7b2b0be9eddb81f28f3dc33a2d8
Yashu7 <janmalawski2@gmail.com>
2024-09-01 22:36:09 +0200

    Appium

--- TestPathDesigner.App/ViewModels/MainPageViewModel.cs
+++ TestPathDesigner.App/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TestPathDesigner.Testing;
 
 namespace TestPathDesigner.App.ViewModels
 {
@@ -21,9 +24,36 @@ namespace TestPathDesigner.App.ViewModels
                 return _title;
             }
         }
+        private string _appName;
+        public string AppName
+        {
+            set
+            {
+                _appName = value;
+                OnPropertyChanged(nameof(AppName));
+            }
+            get
+            {
+                return _appName;
+            }
+        }
+        public ICommand StartTesting { get; set; }
         public MainPageViewModel()
         {
             Title = "Hello world";
+            StartTesting = new RelayCommand(() =>
+            {
+                var appiumTest = new AppiumTesting(AppName);
+                List<string> elements = new List<string>()
+                {
+                    "Jeden",
+                    "Plus",
+                    "Dwa",
+                    "Równa się"
+                };
+                appiumTest.StartTesting(elements);
+            });
         }
+
     }
 }
---
+++ TestPathDesigner.Testing/AppiumTesting.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPathDesigner.Testing
+{
+    public class AppiumTesting
+    {
+        private string appId;
+        private WindowsDriver<WindowsElement> driver;
+        public AppiumTesting(string appName)
+        {
+            appId = appName;
+            var appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", appId);
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+        }
+        public void StartTesting(List<string> elements)
+        {
+            foreach(var element in elements)
+            {
+                var e = driver.FindElementByName(element);
+                e.Click();
+            }
+        }
+    }
+}
