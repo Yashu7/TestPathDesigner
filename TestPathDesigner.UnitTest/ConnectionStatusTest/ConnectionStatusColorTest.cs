b41b9373a07c7d7cb0705a8becd2e1ad33eafcd2
Yashu7 <janmalawski2@gmail.com>
2024-09-04 18:28:38 +0200

    Added unit test project

---
+++ TestPathDesigner.UnitTest/ConnectionStatusTest/ConnectionStatusColorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using TestPathDesigner.ConnectionStatusLibrary.Converters;
+using TestPathDesigner.ConnectionStatusLibrary.Enums;
+
+namespace TestPathDesigner.UnitTest.ConnectionStatusTest
+{
+    [TestClass]
+    public class ConnectionStatusColorTest
+    {
+        [TestMethod]
+        public void ConnectionStatusConnectedReturnBlue()
+        {
+            ConnectionStatusEnum connectionStatus = ConnectionStatusEnum.Connected;
+            ConnectionStatusToColorConverter converter = new ConnectionStatusToColorConverter();
+            var color = converter.Convert(connectionStatus, null,null,null);
+            Assert.AreEqual(color, Brushes.Blue);
+        }
+    }
+}
